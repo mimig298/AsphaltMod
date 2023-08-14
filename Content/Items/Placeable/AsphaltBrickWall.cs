8dc2274cbc168e1818ff5d797d90842c6789c516
mimig298 <miguelldvmendes@gmail.com>
2023-08-14 11:53:08 +0100

    0.10.2.1: Asphalt Brick Wall

---
+++ Content/Items/Placeable/AsphaltBrickWall.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AsphaltMod.Content.Items.Placeable
+{
+    public class AsphaltBrickWall : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 400;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableWall(ModContent.WallType<Walls.AsphaltBrickWall>());
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(4)
+                .AddIngredient<AsphaltBricks>()
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+}
---
+++ Content/Walls/AsphaltBrickWall.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AsphaltMod.Content.Walls
+{
+    public class AsphaltBrickWall : ModWall
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.wallHouse[Type] = true;
+
+            DustType = DustID.Asphalt;
+            AddMapEntry(new Color(21, 21, 22));
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}
