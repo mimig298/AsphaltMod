03a683068ef7cb5cd698366ffc418ae8aa53d76a
mimig298 <miguelldvmendes@gmail.com>
2023-08-14 20:18:22 +0100

    0.10.3.1: Asphalt Table & Localization Patches

---
+++ Content/Items/Placeable/Furniture/AsphaltTable.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AsphaltMod.Content.Items.Placeable.Furniture
+{
+    public class AsphaltTable : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.AsphaltTable>());
+            Item.width = 30;
+            Item.height = 22;
+            Item.value = Item.sellPrice(copper: 60);
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.AsphaltBlock, 8)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+}
---
+++ Content/Tiles/Furniture/AsphaltTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.Localization;
+
+namespace AsphaltMod.Content.Tiles.Furniture
+{
+    public class AsphaltTable : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileTable[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            TileID.Sets.IgnoredByNpcStepUp[Type] = true;
+
+            DustType = DustID.Asphalt;
+            AdjTiles = new int[] { TileID.Tables };
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
+            TileObjectData.addTile(Type);
+
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+            AddMapEntry(new Color(20, 20, 20), Language.GetText("MapObject.Table"));
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}
