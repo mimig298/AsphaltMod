047d1e03cbc53026a7831fe1716cab715c086ca3
mimig298 <miguelldvmendes@gmail.com>
2023-08-15 12:28:24 +0100

    0.10.5.1: Asphalt Work Bench

---
+++ Content/Items/Placeable/Furniture/AsphaltWorkbench.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AsphaltMod.Content.Items.Placeable.Furniture
+{
+    public class AsphaltWorkbench : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.AsphaltWorkbench>());
+            Item.width = 32;
+            Item.height = 18;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.AsphaltBlock, 10)
+                .Register();
+        }
+    }
+}
---
+++ Content/Tiles/Furniture/AsphaltWorkbench.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.Localization;
+
+namespace AsphaltMod.Content.Tiles.Furniture
+{
+    public class AsphaltWorkbench : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileTable[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            TileID.Sets.IgnoredByNpcStepUp[Type] = true;
+
+            DustType = DustID.Asphalt;
+            AdjTiles = new int[] { TileID.WorkBenches };
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
+            TileObjectData.newTile.CoordinateHeights = new[] { 18 };
+            TileObjectData.addTile(Type);
+
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+            AddMapEntry(new Color(53, 53, 47), Language.GetText("ItemName.WorkBench"));
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}
