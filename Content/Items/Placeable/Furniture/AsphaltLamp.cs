3c25e38f0bf601e4d5724a55e9afd40a6a56191a
mimig298 <miguelldvmendes@gmail.com>
2023-08-16 18:19:44 +0100

    0.10.7.1, The most painful update so far: Asphalt Lamp AND MOD ICON FIX!!!

---
+++ Content/Items/Placeable/Furniture/AsphaltLamp.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace AsphaltMod.Content.Items.Placeable.Furniture
+{
+    public class AsphaltLamp : ModItem
+    {
+        public override LocalizedText Tooltip => LocalizedText.Empty;
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.AsphaltLamp>());
+            Item.width = 10;
+            Item.height = 30;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.AsphaltBlock, 3)
+                .AddIngredient(ItemID.Torch)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+}
