e12fb9e25d74409f084ca9cc12769497e36ff190
mimig298 <miguelldvmendes@gmail.com>
2026-04-26 18:57:54 +0100

    Asphalt hook

---
+++ Content/Items/Equippable/AsphaltHook.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AsphaltMod.Content.Items.Equippable;
+
+public class AsphaltHook : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.width = 20;
+        Item.height = 28;
+
+        Item.useTime = 10;
+        Item.useAnimation = 10;
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.noUseGraphic = true;
+
+        Item.UseSound = SoundID.Item1;
+
+        Item.value = Item.sellPrice(gold: 1);
+        Item.rare = ItemRarityID.LightRed;
+
+        Item.shoot = ModContent.ProjectileType<Projectiles.AsphaltHook>();
+        Item.shootSpeed = 14f;
+
+        if (ModLoader.TryGetMod("HookStatsAndWingStats", out Mod hookStatsMod) && ContentSamples.ProjectilesByType.TryGetValue(Item.shoot, out Projectile hookProj))
+        {
+            float hookReach = hookProj.ModProjectile?.GrappleRange() ?? 440f;
+            float hookSpeed = Item.shootSpeed * (hookProj.extraUpdates + 1);
+            hookStatsMod.Call("SetHookStats", Type, hookReach, hookSpeed, 1, (byte)0);
+        }
+    }
+
+    public override void AddRecipes()
+    {
+        CreateRecipe()
+            .AddIngredient(ItemID.GrapplingHook)
+            .AddIngredient(ItemID.AsphaltBlock, 20)
+            .AddTile(TileID.MythrilAnvil)
+            .AddTile(TileID.Blendomatic)
+            .Register();
+    }
+}
---
+++ Content/Projectiles/AsphaltHook.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AsphaltMod.Content.Projectiles;
+
+public class AsphaltHook : ModProjectile
+{
+    private static Asset<Texture2D> chainTexture;
+
+    public override void Load()
+    {
+        chainTexture = ModContent.Request<Texture2D>(Texture + "_Chain");
+    }
+
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.SingleGrappleHook[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        Projectile.CloneDefaults(ProjectileID.GemHookAmethyst);
+        Projectile.extraUpdates = 1;
+    }
+
+    public override float GrappleRange()
+    {
+        return 440f;
+    }
+
+    public override void NumGrappleHooks(Player player, ref int numHooks)
+    {
+        numHooks = 1;
+    }
+
+    public override void GrappleRetreatSpeed(Player player, ref float speed)
+    {
+        speed = 22f;
+    }
+
+    public override void GrapplePullSpeed(Player player, ref float speed)
+    {
+        speed = 16f;
+    }
+
+    // exmaple mod
+    public override bool PreDrawExtras()
+    {
+        Vector2 playerCenter = Main.player[Projectile.owner].MountedCenter;
+        Vector2 center = Projectile.Center;
+        Vector2 directionToPlayer = playerCenter - Projectile.Center;
+        float chainRotation = directionToPlayer.ToRotation() - MathHelper.PiOver2;
+        float distanceToPlayer = directionToPlayer.Length();
+
+        while (distanceToPlayer > 16f && !float.IsNaN(distanceToPlayer))
+        {
+            directionToPlayer /= distanceToPlayer; // get unit vector
+            directionToPlayer *= chainTexture.Height(); // multiply by chain link length
+
+            center += directionToPlayer; // update draw position
+            directionToPlayer = playerCenter - center; // update distance
+            distanceToPlayer = directionToPlayer.Length();
+
+            Color drawColor = Lighting.GetColor((int)center.X / 16, (int)(center.Y / 16));
+
+            Main.EntitySpriteDraw(chainTexture.Value, center - Main.screenPosition,
+                chainTexture.Value.Bounds, drawColor, chainRotation,
+                chainTexture.Size() * 0.5f, 1f, SpriteEffects.None, 0);
+        }
+
+        return false;
+    }
+}
