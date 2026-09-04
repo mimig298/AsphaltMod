b5e3b59b31d660d4d397a15e4ae7cbfbe9e77216
mimig298 <miguelldvmendes@gmail.com>
2026-09-04 17:10:19 +0100

    clean up chandelier file

--- Content/Tiles/AsphaltChandelier.cs
+++ Content/Tiles/AsphaltChandelier.cs
@@ -2,8 +2,6 @@
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria;
-using Terraria.DataStructures;
-using Terraria.GameContent;
 using Terraria.GameContent.Drawing;
 using Terraria.ID;
 using Terraria.Localization;
@@ -83,36 +81,4 @@ public class AsphaltChandelier : ModTile
         tileFlameData.flameRangeMultX = 0.15f;
         tileFlameData.flameRangeMultY = 0.35f;
     }
-
-    /*
-    public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
-    {
-        Tile tile = Main.tile[i, j];
-
-        if (!TileDrawing.IsVisible(tile))
-            return;
-
-        Vector2 screenPos = Main.Camera.UnscaledPosition;
-        Vector2 screenOffset = Main.drawToScreen ? Vector2.Zero : new(Main.offScreenRange);
-        int width = 16;
-        int height = 16;
-        int tileTop = 0;
-        short frameX = tile.TileFrameX;
-        short frameY = tile.TileFrameY;
-        SpriteEffects spriteEffects = SpriteEffects.None;
-        TileLoader.SetSpriteEffects(i, j, Type, ref spriteEffects);
-        TileLoader.SetDrawPositions(i, j, ref width, ref tileTop, ref height, ref frameX, ref frameY);
-
-        ulong seed = Main.TileFrameSeed ^ (ulong)(((long)i << 32) | (uint)j);
-
-        for (int l = 0; l < 7; l++)
-        {
-            float shakeX = Utils.RandomInt(ref seed, -10, 11) * 0.15f;
-            float shakeY = Utils.RandomInt(ref seed, -10, 1) * 0.35f;
-            Vector2 drawPos = new Vector2(i * 16 - (int)screenPos.X - (width - 16f) / 2f + shakeX, j * 16 - (int)screenPos.Y + tileTop + shakeY) + screenOffset;
-            Rectangle source = new(frameX, frameY, width, height);
-            spriteBatch.Draw(flameTexture.Value, drawPos, source, new Color(100, 100, 100, 0), 0f, Vector2.Zero, 1f, spriteEffects, 0f);
-        }
-    }
-    */
 }
