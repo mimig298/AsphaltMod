2625152ecd601c209e6a58a61140038b089374fe
mimig298 <miguelldvmendes@gmail.com>
2026-09-07 19:42:06 +0100

    piano

---
+++ Content/Items/Placeable/AsphaltPiano.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AsphaltMod.Content.Items.Placeable;
+
+public class AsphaltPiano : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.CloneDefaults(ItemID.Piano);
+        Item.createTile = ModContent.TileType<Tiles.AsphaltPiano>();
+    }
+
+    public override void AddRecipes()
+    {
+        CreateRecipe()
+            .AddIngredient(ItemID.AsphaltBlock, 15)
+            .AddIngredient(ItemID.Bone, 4)
+            .AddIngredient(ItemID.Book)
+            .AddTile<Tiles.AsphaltMachine>()
+            .Register();
+    }
+}
---
+++ Content/Tiles/AsphaltPiano.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace AsphaltMod.Content.Tiles;
+
+public class AsphaltPiano : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileFrameImportant[Type] = true;
+        Main.tileNoAttach[Type] = true;
+        Main.tileTable[Type] = true;
+        Main.tileSolidTop[Type] = true;
+        Main.tileLavaDeath[Type] = true;
+
+        DustType = -1;
+        AdjTiles = [TileID.Pianos];
+
+        TileObjectData.newTile.CopyFrom(TileObjectData.GetTileData(TileID.Pianos, 0));
+        TileObjectData.addTile(Type);
+
+        AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+        AddMapEntry(new Color(127, 92, 69), Language.GetText("ItemName.Piano"));
+    }
+}
