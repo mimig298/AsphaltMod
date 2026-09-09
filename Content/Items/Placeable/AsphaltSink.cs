d1c01f3dd239f948eb0f40cc33c0212bb5def2e8
mimig298 <miguelldvmendes@gmail.com>
2026-09-09 16:52:04 +0100

    Asphalt sink

---
+++ Content/Items/Placeable/AsphaltSink.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AsphaltMod.Content.Items.Placeable;
+
+public class AsphaltSink : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.CloneDefaults(ItemID.MetalSink);
+        Item.createTile = ModContent.TileType<Tiles.AsphaltSink>();
+        Item.placeStyle = 0;
+    }
+
+    public override void AddRecipes()
+    {
+        CreateRecipe()
+            .AddIngredient(ItemID.AsphaltBlock, 6)
+            .AddIngredient(ItemID.WaterBucket)
+            .AddTile<Tiles.AsphaltMachine>()
+            .Register();
+    }
+}
---
+++ Content/Tiles/AsphaltSink.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace AsphaltMod.Content.Tiles;
+
+public class AsphaltSink : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileFrameImportant[Type] = true;
+        Main.tileLavaDeath[Type] = true;
+        TileID.Sets.InteractibleByNPCs[Type] = true;
+        TileID.Sets.CountsAsWaterSource[Type] = true;
+
+        DustType = -1;
+        AdjTiles = [TileID.Sinks];
+
+        TileObjectData.newTile.CopyFrom(TileObjectData.GetTileData(TileID.Sinks, 0));
+        TileObjectData.addTile(Type);
+
+        AddMapEntry(new Color(105, 107, 125), Language.GetText("MapObject.Sink"));
+    }
+}
